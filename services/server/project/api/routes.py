1db3fe92b7f99fdcad830b6f5aab7e7105d3c467
Sergey Gavrilenko <gryroach@gmail.com>
2022-01-15 22:44:45 +0300

    Create simple GET documents

--- services/server/project/api/routes.py
+++ services/server/project/api/routes.py
@@ -1,4 +1,5 @@
-from flask import current_app as app
+from flask import current_app as app, request, jsonify
+from .models import Documents, Rights
 
 
 @app.route('/', methods=['GET'])
@@ -6,3 +7,9 @@ def hello_world():
     return {
         'hello': 'world'
     }
+
+
+@app.route('/documents/', methods=['GET', 'POST'])
+def documents_list():
+    documents = [document.to_json() for document in Documents.query.all()]
+    return jsonify(documents)
