eb234f086aec92c3101ba2ea9ecdf3a64ed22c5f
Sergey Gavrilenko <gryroach@gmail.com>
2022-01-15 23:23:53 +0300

    Create POST request; Modify models methods

--- services/server/project/api/models.py
+++ services/server/project/api/models.py
@@ -15,9 +15,20 @@ class Documents(db.Model):
     inserted_at = db.Column(db.DateTime, default=db.func.now())
     updated_at = db.Column(db.DateTime, default=db.func.now())
 
-    def __init__(self, name, text):
+    def __init__(self, name, text, inserted_at, updated_at):
         self.name = name
         self.text = text
+        self.inserted_at = inserted_at
+        self.updated_at = updated_at
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'text': self.text,
+            'inserted_at': self.inserted_at,
+            'updated_at': self.updated_at
+        }
 
     def __str__(self):
         return f"<documents {self.id}>"
--- services/server/project/api/routes.py
+++ services/server/project/api/routes.py
@@ -1,5 +1,9 @@
+import datetime
+
 from flask import current_app as app, request, jsonify
 from .models import Documents, Rights
+from datetime import datetime
+from .. import db
 
 
 @app.route('/', methods=['GET'])
@@ -10,6 +14,18 @@ def hello_world():
 
 
 @app.route('/documents/', methods=['GET', 'POST'])
-def documents_list():
-    documents = [document.to_json() for document in Documents.query.all()]
-    return jsonify(documents)
+def documents():
+    response = {'status': 'success'}
+    if request.method == 'POST':
+        post_data = request.get_json()
+        print(post_data)
+        name = post_data.get('name')
+        text = post_data.get('text')
+        inserted_at = datetime.now()
+        updated_at = datetime.now()
+        db.session.add(Documents(name=name, text=text, inserted_at=inserted_at, updated_at=updated_at))
+        db.session.commit()
+        response['message'] = 'Document added!'
+    else:
+        response['documents'] = [document.to_json() for document in Documents.query.all()]
+    return jsonify(response)
